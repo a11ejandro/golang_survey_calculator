2b0664f5b1f255aa710c6acecca956d44e65e825
a11ejandro <bendthe@gmail.com>
2018-07-26 18:27:52 +0300

    Update worker to perform calculations and track statistic

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func init() {
+  connectToDb()
   registerWorker()
 }
 
--- worker.go
+++ worker.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-  "fmt"
   "os"
+  "fmt"
+  "time"
+  "database/sql"
   "github.com/benmanns/goworker"
 )
 
@@ -22,8 +24,50 @@ func registerWorker() {
 }
 
 func surveyCalculatorWorker(queue string, args ...interface{}) error {
-  taskId := int64(args[0].(float64))
-  fmt.Println("Recieved task with id ", taskId)
+  startTime := time.Now()
 
+  taskId := sql.NullInt64{Int64: int64(args[0].(float64)), Valid: true}
+  task := new(Task).Find(taskId)
+
+  surveyResults := new(SurveyResult).Page(task.page, task.perPage)
+  avg := avgForSurvey(surveyResults)
+
+  // create new survey result which stores the result value of current survey
+  newResult := new(SurveyResult)
+  newResult.value = avg
+  newResult.taskId = taskId
+  newResult.Save()
+  endTime := time.Now()
+  duration := endTime.Sub(startTime)
+
+  // write statistic to db
+  statistic := new(Statistic)
+  statistic.duration = duration.Seconds()
+  statistic.taskId = taskId
+  statistic.handlerType = "go_worker"
+  statistic.collectionSize = task.perPage
+
+  statistic.Save()
+  fmt.Println("Completed task", taskId.Int64)
   return nil
+}
+
+func avgForSurvey(surveyResults []*SurveyResult) (float64) {
+  min, max, sum := 0.0, 0.0, 0.0
+
+  for _, element := range surveyResults {
+      if element.value < min {
+        min = element.value
+      }
+
+      if element.value > max {
+        max = element.value
+      }
+
+      sum = sum + element.value
+  }
+
+  avg := sum / float64(len(surveyResults))
+
+  return (min + max + avg) / 3
 }
